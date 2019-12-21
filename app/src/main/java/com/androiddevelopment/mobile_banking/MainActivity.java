79060f3c6365935e8bb653ddd39ada60b5f33127
Fanny Agustiawan <fannyagustiawan@gmail.com>
2019-12-21 17:38:44 +0700

    commit

--- app/src/main/java/com/androiddevelopment/mobile_banking/MainActivity.java
+++ app/src/main/java/com/androiddevelopment/mobile_banking/MainActivity.java
@@ -1,6 +1,8 @@
 package com.androiddevelopment.mobile_banking;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -8,60 +10,107 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.androiddevelopment.mobile_banking.helper.InputValidation;
+import com.androiddevelopment.mobile_banking.sql.DatabaseHelper;
 
 
 public class MainActivity extends AppCompatActivity {
     //declare vars
+    private final AppCompatActivity activity = MainActivity.this;
     String UserName, Password;
+
+    private TextInputLayout textInputLayoutEmail;
+    private TextInputLayout textInputLayoutPassword;
+    EditText textInputEditTextEmail;
+    EditText textInputEditTextPassword;
+
+    Button myButton;
+
     int loginCntr = 3;
+
+    private InputValidation inputValidation;
+    private DatabaseHelper databaseHelper;
+
+    private void initObjects() {
+        databaseHelper = new DatabaseHelper(activity);
+        inputValidation = new InputValidation(activity);
+    }
+
+    private void initViews() {
+        //reference Button, User Name and Password
+        myButton = (Button) findViewById(R.id.loginButton);
+
+        textInputLayoutEmail = (TextInputLayout) findViewById(R.id.usernameTextView);
+        textInputLayoutPassword = (TextInputLayout) findViewById(R.id.passwordTextView);
+
+        textInputEditTextEmail = (EditText) findViewById(R.id.usernameEditText);
+        textInputEditTextPassword = (EditText) findViewById(R.id.passwordEditText);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        initObjects();
+        initViews();
 
         //display welcome msg when app starts
-        Toast.makeText(MainActivity.this, "Welcome User. Please enter your Username and Password.", Toast.LENGTH_LONG).show();
-
-        //reference Button, User Name and Password
-        Button myButton = (Button) findViewById(R.id.loginButton);
-        final EditText myUserName = (EditText) findViewById(R.id.usernameEditText);
-        final EditText myPassword = (EditText) findViewById(R.id.passwordEditText);
+        Toast.makeText(activity, "Welcome User. Please enter your Username and Password.", Toast.LENGTH_LONG).show();
 
         //register button with Event Listener class, and Event handler method
         myButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 //get user input
-                UserName = myUserName.getText().toString();
-                Password = myPassword.getText().toString();
-
+                UserName = textInputEditTextEmail.getText().toString();
+                Password = textInputEditTextPassword.getText().toString();
 
                 //check how many times unregistered user tried to log in with wrong credentials
 
-                //check if username and password are correct
-                if (UserName.equals("1") && Password.equals("1")) {
-                    //user is registered, display menu layout and registered user msg
-
-                    Intent myIntent = new Intent(MainActivity.this, MenuActivity.class);
-                    myIntent.putExtra("stringReference", "Access Granted!");
-                    //display menu activity screen
-                    startActivity(myIntent);
-
-                }//end if
-                //check login attempts counter
-                else if (loginCntr != 1) {
-                    //unregistered user, display unregistered user msg and decrease login counter
-                    loginCntr = loginCntr - 1;
-
-                    Toast.makeText(MainActivity.this, "Access Denied! Please try again.You have " + loginCntr + " attempt(s) remaining", Toast.LENGTH_LONG).show();
-                }//end else if
-                else {
-                    //3 login attempts are up, close app
-                    Toast.makeText(MainActivity.this, "Access Denied! Closing app!", Toast.LENGTH_LONG).show();
-                    finish();
-                }//end else
+                verifyFromSQLite();
 
             }//end onClick
         });//end setOnClickListener
     }//end onCreate
+
+    private void verifyFromSQLite() {
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextEmail, textInputLayoutEmail, getString(R.string.error_message_email))) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextEmail(textInputEditTextEmail, textInputLayoutEmail, getString(R.string.error_message_email))) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextPassword, textInputLayoutPassword, getString(R.string.error_message_email))) {
+            return;
+        }
+
+        if (databaseHelper.checkUser(textInputEditTextEmail.getText().toString().trim()
+                , textInputEditTextPassword.getText().toString().trim())) {
+
+
+            Intent myIntent = new Intent(MainActivity.this, MenuActivity.class);
+            myIntent.putExtra("stringReference", "Access Granted!");
+            //display menu activity screen
+            startActivity(myIntent);
+            emptyInputEditText();
+
+
+        } else if (loginCntr != 1) {
+            //unregistered user, display unregistered user msg and decrease login counter
+            loginCntr = loginCntr - 1;
+
+            Toast.makeText(activity, "Access Denied! Please try again.You have " + loginCntr + " attempt(s) remaining", Toast.LENGTH_LONG).show();
+        }//end else if
+        else {
+            //3 login attempts are up, close app
+            Toast.makeText(activity, "Access Denied! Closing app!", Toast.LENGTH_LONG).show();
+            finish();
+        }//end else
+    }
+
+    private void emptyInputEditText() {
+        textInputEditTextEmail.setText(null);
+        textInputEditTextPassword.setText(null);
+    }
 }//end MainActivity
